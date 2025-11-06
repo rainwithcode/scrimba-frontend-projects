e66fa74c2eda5bc4d20b2b99a0f78b8b52f4988c
Rain <rainkalugdan@gmail.com>
2025-11-06 16:45:09 +0800

    fix(rain-business-card-v2): correct linkedin link

--- react-basics/rain-business-card-v2/src/App.jsx
+++ react-basics/rain-business-card-v2/src/App.jsx
@@ -5,15 +5,15 @@ import avatarUrl from "./assets/images/rain.jpg";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Info
         avatar={avatarUrl}
         name="Rain"
         website="https://rainkalugdan.vercel.app/"
         email="rainkalugdan@gmail.com"
-        linkedin="linkedin.com/in/rain-kalugdan-98a457301"
+        linkedin="https://www.linkedin.com/in/rain-kalugdan-98a457301/"
       />
-    </>
+    </div>
   );
 }
 
