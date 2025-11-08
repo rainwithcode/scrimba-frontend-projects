034472f8089173a72af58c43424db4a1267473a8
Rain <rainkalugdan@gmail.com>
2025-11-08 12:37:30 +0800

    feat(rain-business-card-v2): add About component

--- react-basics/rain-business-card-v2/src/App.jsx
+++ react-basics/rain-business-card-v2/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Info from "./components/Info";
+import About from "./components/About";
 import avatarUrl from "./assets/images/rain.jpg";
 
 function App() {
@@ -12,6 +13,10 @@ function App() {
         website="https://rainkalugdan.vercel.app/"
         email="rainkalugdan@gmail.com"
         linkedin="https://www.linkedin.com/in/rain-kalugdan-98a457301/"
+      />
+      <About
+        description="As a frontend developer, I’m passionate about creating accessible, minimalist web experiences that make everyday life simpler. 
+      I believe that organized systems are essential for productivity and growth, and I’m always looking for new ways to make digital experiences easier and more enjoyable."
       />
     </div>
   );
---
+++ react-basics/rain-business-card-v2/src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About({ description }) {
+  return (
+    <section className="section">
+      <h2 className="section-title">About</h2>
+      <p>{description}</p>
+    </section>
+  );
+}
