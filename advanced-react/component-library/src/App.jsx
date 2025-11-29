e0f79c14252cd860e3ece9c9049cc0b8f6f54533
Rain <rainkalugdan@gmail.com>
2025-11-29 14:49:04 +0800

    feat(component-library): add header

--- advanced-react/component-library/src/App.jsx
+++ advanced-react/component-library/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <>Component Library</>;
+  return <Header />;
 }
 
 export default App;
