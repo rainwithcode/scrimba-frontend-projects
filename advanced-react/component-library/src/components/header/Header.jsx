03748c3c2a02035a87f1dac959d7bfbb0f7d50b8
Rain <rainkalugdan@gmail.com>
2025-11-29 17:26:25 +0800

    feat(component-library): move header files to header folder

---
+++ advanced-react/component-library/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <header className="container">
+      <h1 className="site-title">Component Library++</h1>
+      <p className="site-description">
+        Reusable, stylish UI components including badges, banners, cards, and
+        testimonials with clean design and easy integration.
+      </p>
+    </header>
+  );
+}
