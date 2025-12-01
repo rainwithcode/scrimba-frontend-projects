f33189d9464c7a58fd45b16558377c047299f91b
Rain <rainkalugdan@gmail.com>
2025-12-01 11:34:32 +0800

    feat(component-library): add header class

--- advanced-react/component-library/src/components/header/Header.jsx
+++ advanced-react/component-library/src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import "./header.css";
 
 export default function Header() {
   return (
-    <header className="container">
+    <header className="header container">
       <h1 className="site-title">Component Library++</h1>
       <p className="site-description">
         Reusable, stylish UI components including badges, banners, cards, and
