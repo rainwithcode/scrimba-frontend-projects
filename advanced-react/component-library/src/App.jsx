56fb208f0d36bb2799aa8285d9907945a450bde3
Rain <rainkalugdan@gmail.com>
2025-12-03 12:32:58 +0800

    feat(component-library): display banners

--- advanced-react/component-library/src/App.jsx
+++ advanced-react/component-library/src/App.jsx
@@ -20,6 +20,35 @@ export default function App() {
 
   const badgeVariants = ["square", "pill"];
 
+  const banners = [
+    {
+      status: "success",
+      title: "Congratulations!",
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.",
+    },
+    {
+      status: "warning",
+      title: "Attention",
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.",
+    },
+    {
+      status: "error",
+      title: "There is a problem with your application",
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.",
+    },
+    {
+      status: "neutral",
+      title: "Update available",
+      message:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.",
+    },
+  ];
+
+  const bannerVariants = ["multiline", "single"];
+
   return (
     <div className="container">
       <Header />
@@ -37,7 +66,30 @@ export default function App() {
                 <Badge
                   shapeVariant={shapeVariant}
                   color={color}
-                  key={`${color}-${shapeVariant}`}
+                  key={`badge-${shapeVariant}-${color}`}
+                />
+              ))}
+            </div>
+          </div>
+        ))}
+        {/* --- Banners --- */}
+        {/* Display multiline and single banners */}
+        {bannerVariants.map((lineVariant) => (
+          <div>
+            <h2 className="component-title">
+              {lineVariant.charAt(0).toUpperCase() + lineVariant.slice(1)}{" "}
+              Banners
+            </h2>
+            <div className="banner-grid">
+              {banners.map((banner) => (
+                <Banner
+                  variant={lineVariant}
+                  status={banner.status}
+                  title={banner.title}
+                  {...(lineVariant === "multiline"
+                    ? { message: banner.message }
+                    : {})}
+                  key={`banner-${lineVariant}-${banner.status}`}
                 />
               ))}
             </div>
---
+++ advanced-react/component-library/src/components/banner/Banner.jsx
@@ -0,0 +1,30 @@
+import ErrorIcon from "../../assets/error-icon.svg";
+import InfoIcon from "../../assets/info-icon.svg";
+import SuccessIcon from "../../assets/success-icon.svg";
+import WarningIcon from "../../assets/warning-icon.svg";
+
+import "./banner.css";
+
+const defaultIcons = {
+  error: ErrorIcon,
+  neutral: InfoIcon,
+  success: SuccessIcon,
+  warning: WarningIcon,
+};
+
+export default function Banner({
+  variant = "single",
+  status = "neutral",
+  title = "Update available",
+  message,
+}) {
+  return (
+    <div className={`banner ${variant} ${status}`}>
+      <div className="banner-header">
+        <img src={defaultIcons[status]} aria-hidden="true" />
+        <span className="banner-title">{title}</span>
+      </div>
+      {message && <p className="banner-message">{message}</p>}
+    </div>
+  );
+}
